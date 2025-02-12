26ecfc8a8016d1aeadef392be902ec296a2b5f1b
Honda Masahiro <hondams@gmail.com>
2025-02-12 14:39:33 +0900

    add build setup emsdk for zscaler

---
+++ tools/emsdk_patch/generate_emsdk_patch.sh
@@ -0,0 +1,3 @@
+cd ..
+diff -c emsdk/emsdk.py emsdk_patch/emsdk.py.updated > emsdk_patch/emsdk.py.patch
+dos2unix.exe emsdk_patch/emsdk.py.patch
---
+++ tools/emsdk_patch/patch_emsdk.sh
@@ -0,0 +1,2 @@
+cd ..
+patch emsdk/emsdk.py emsdk_patch/emsdk.py.patch
